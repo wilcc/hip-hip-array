b566a5298ae59ba26ba766c0db55318f9750f5c3
Wilson Chen <wilson.chen@codeimmersives.com>
2020-02-25 23:53:07 -0500

    finish

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ makeBanner(2)
 // Challenge 2
 // Loop through the following array BACKWARDS, console.logging out each value. 64 should be first and 100 last.
 const grades = [100, 80, 110, 75, 83, 64];
-for (let i = grades.length-1; i > 0; i-- ){
+for (let i = grades.length-1; i >= 0; i-- ){
   console.log(grades[i])
 }
 makeBanner(3)
